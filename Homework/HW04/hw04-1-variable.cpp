af4ee927e4ae180461fbda3ab4b6407f9ef811a0
dodrjune <dodrjune@gmail.com>
2023-08-15 15:10:58 +0700

    4-1

--- Homework/HW04/hw04-1-variable.cpp
+++ Homework/HW04/hw04-1-variable.cpp
@@ -8,7 +8,7 @@
 int main() {
 int a = 10 ;
 float b = 20 ;
-char c [] = " Haha " ;
+char c [] = "Haha" ;
 char d = 'a' ;
 printf ( " %d %.1f %s %c ", a, b, c, d) ; //TODO: #10 Display variable a, b, c and d.
 return 0 ; }
